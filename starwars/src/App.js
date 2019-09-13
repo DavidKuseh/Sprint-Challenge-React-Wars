d3165b69e15b0b81ed45a7938bf090f7eef595d1
David Kuseh <davegru203@gmail.com>
2019-09-13 08:55:04 +0000

    made changes to state and created Characters.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import './App.css';
+import Characters from "./components/Characters";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  const [name, setName] = useState("");
-  const [height, setHeight] = useState();
-  const [mass, setMass] = useState();
-  const [birthYear, setBirthYear] = useState();
-  
+  const [people, setPeople] = useState([]);
+
+
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -17,8 +17,10 @@ const App = () => {
   useEffect(()=> {
     axios.get("https://swapi.co/api/people")
       .then(response => {
-        setName(response.data.name);
-        setheight
+        setPeople(response.data.results);
+      })
+      .catch(error =>{
+        console.log(error);
       })
   })
 
@@ -26,6 +28,13 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      {
+           
+            // results.name.map(()=> {
+            //     <Characters ></Characters>
+            // })
+         
+      }
     </div>
   );
 }
---
+++ starwars/src/components/Characters.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function Characters ({name, height, mass, birthYear}) {
+    return (
+        <>
+        <div>
+            {
+                // people.results.map(() => {
+                //     name={name}
+                //     height={height}
+                //     mass={mass}
+                //     birthYear={birthYear}
+                // })
+            }
+        </div>
+        </> 
+    )
+}
+
+export default Characters;
